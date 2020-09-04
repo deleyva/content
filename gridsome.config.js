024ca7ab37ed545fbb312ec0c7efdc5daa3063cb
deleyva <jesuslopezdeleyva@gmail.com>
2020-09-04 11:59:24 +0200

    edito siteName y siteDescription

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,31 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'CATEDU',
+  siteDescription: 'El Centro Aragonés de Tecnologías para la Educación (CATEDU) depende del Departamento de Educación, Cultura y Deporte del Gobierno de Aragón: Es Centro de Profesorado específico de formación a distancia del profesorado y ofrece servicios destinados a incentivar y facilitar el uso de las TIC en la educación.',
 
   templates: {
     Post: '/:title',
     Tag: '/tag/:id'
   },
 
-  plugins: [
-    {
-      // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
-        refs: {
-          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
-          tags: {
-            typeName: 'Tag',
-            create: true
-          }
+  plugins: [{
+    // Create posts from markdown files
+    use: '@gridsome/source-filesystem',
+    options: {
+      typeName: 'Post',
+      path: 'content/posts/*.md',
+      refs: {
+        // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+        tags: {
+          typeName: 'Tag',
+          create: true
         }
       }
     }
-  ],
+  }],
 
   transformers: {
     //Add markdown support to all file-system sources
