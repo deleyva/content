f98e7a030309d07ab6fd9b1b6ad3507f58d892bd
deleyva <jesuslopezdeleyva@gmail.com>
2020-09-04 12:09:53 +0200

    Cambio author intro

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -6,7 +6,7 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.
+			Aquí iría la intro del autor.
 		</p>
 
 		<p class="author__links">
@@ -27,39 +27,39 @@ query {
 
 <script>
 export default {
-	props: ['showTitle']
-}
+  props: ["showTitle"],
+};
 </script>
 
 <style lang="scss">
 .author {
-	margin: 0 auto;
-	max-width: 500px;
-	text-align: center;
-	padding-bottom: calc(var(--space) / 2);
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding-bottom: calc(var(--space) / 2);
 
-	&__image {
-		border-radius: 100%;
-		width: 90px;
-		height: 90px;
-		margin-bottom: 1em;
-	}
+  &__image {
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 1em;
+  }
 
-	&__intro {
-		opacity: .8;
-	}
+  &__intro {
+    opacity: 0.8;
+  }
 
-	&__site-title {
-		margin: 0 auto;
-		font-size: 1.5em;
-		max-width: 400px;
-	}
+  &__site-title {
+    margin: 0 auto;
+    font-size: 1.5em;
+    max-width: 400px;
+  }
 
-	&__links {
-		margin-top: -.5em;
-		a {
-			margin: 0 .5em;
-		}
-	}
+  &__links {
+    margin-top: -0.5em;
+    a {
+      margin: 0 0.5em;
+    }
+  }
 }
 </style>
